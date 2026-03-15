f915eaebc250a4440a404ef919c2a2437d8065f3
Wei FeiYang <wesley.f.young@outlook.com>
2026-03-15 17:29:32 +0800

    fix: unexpected required param inferred from endpoint with no input

--- src/client/endpoint.ts
+++ src/client/endpoint.ts
@@ -51,7 +51,10 @@ export type MilkyClient = MilkyFetch & {
   [K in keyof MilkyClientEndpointNames]: {
     [M in keyof MilkyClientEndpointNames[K]]:
     MilkyClientEndpointNames[K][M] extends infer K extends keyof MilkyRawEndpoints
-      ? (param: Parameters<MilkyRawEndpoints[K]>[0], options?: MilkyFetchOptions) => Promise<ReturnType<MilkyRawEndpoints[K]>> : never
+      ? Parameters<MilkyRawEndpoints[K]> extends [param?: null | undefined]
+        ? (param?: null | undefined, options?: MilkyFetchOptions) => Promise<ReturnType<MilkyRawEndpoints[K]>>
+        : (param: Parameters<MilkyRawEndpoints[K]>[0], options?: MilkyFetchOptions) => Promise<ReturnType<MilkyRawEndpoints[K]>>
+      : never
   } & {
     readonly name: K
   } & {}
